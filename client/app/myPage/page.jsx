686b65505406ff076e263ea9b9ec90b11f3dc217
Jin Choi <jinchoi@Jinui-MacBookAir.local>
2023-05-21 10:36:55 +0900

    add: styling style/#4

---
+++ client/app/myPage/page.jsx
@@ -0,0 +1,37 @@
+/**
+ * @author    : gogleset
+ * @description : 내 정보를 볼 수 있는 페이지 컴포넌트 입니다.
+ **/
+"use client";
+import React from "react";
+// import { useRouter } from "next/navigation";
+// import { useAccount } from "wagmi";
+import $MyPage from "./style";
+// import { boolCheckCookie } from "../utils/cookie";
+import MyPageProfile from "../components/myPageProfile/myPageProfile";
+import MyPageCollection from "../components/MyPageCollection/MyPageCollection";
+
+const page = () => {
+  // const account = useAccount();
+  // const router = useRouter();
+  // console.log(account);
+  // const { isConnected } = account;
+
+  // const checkId = boolCheckCookie("id");
+  // console.log(checkId && isConnected);
+  // 분기 처리
+  // if (isConnected && checkId) {
+  //   return <$MyPage>page</$MyPage>;
+  // }
+  // alert("no account");
+  // return router.push("/");
+
+  return (
+    <$MyPage>
+      <MyPageProfile />
+      <MyPageCollection />
+    </$MyPage>
+  );
+};
+
+export default page;
